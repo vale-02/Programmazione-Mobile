6a649d866d93d57c32322af4bd68502894549964
Valeria <mastrogiorgio.valeria45@gmail.com>
2024-11-02 17:34:11 +0100

    Implementazione grafica di visualizzazione del singolo esame

--- brainiac/lib/view/workplace_viewexam.dart
+++ brainiac/lib/view/workplace_viewexam.dart
@@ -1,8 +1,10 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:brainiac/book/book_screen.dart';
 import 'package:brainiac/workplace/workplace_editexam.dart';
 import 'package:brainiac/youtube/youtube_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:hugeicons/hugeicons.dart';
 
 // ignore: must_be_immutable
 class WorkplaceViewexam extends StatefulWidget {
@@ -30,78 +32,223 @@ class WorkplaceViewexam extends StatefulWidget {
 }
 
 // Pagina di visualizzazione dell'esame selezionato con il GestureDetector in ListExam
-// ** da aggiungere le miniature video e libri prese con API **
 class _WorkplaceViewexam extends State<WorkplaceViewexam> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          fontSize: 20,
+          fontFamily: 'Museo Moderno',
+        ),
+        title: ShaderMask(
+          shaderCallback: (bounds) => LinearGradient(
+            colors: [
+              Color(0xFFFC8D0A),
+              Color(0xFFFE2C8D),
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ).createShader(bounds),
+          child: AutoSizeText(
+            widget.name,
+            maxLines: 2,
+            minFontSize: 13,
+            stepGranularity: 1,
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              _editExamScreen(context);
+            },
+            icon: HugeIcon(
+              icon: HugeIcons.strokeRoundedPencilEdit02,
+              color: Color(0xFFFC8D0A),
+            ),
+          ),
+        ],
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Row(
-              children: [
-                IconButton(
-                    onPressed: () {
-                      _editExamScreen(context);
-                    },
-                    icon: Icon(Icons.edit)),
-                SizedBox(
-                  width: 20,
-                ),
-                Text(widget.name),
-                SizedBox(
-                  width: 20,
-                ),
-                Text(_getGrade()),
-              ],
-            ),
             SizedBox(
               height: 20,
             ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text('CFU: ${widget.cfu.toString()}'),
-                SizedBox(
-                  width: 50,
+                Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: Text(
+                        widget.cfu.toString(),
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Museo Moderno',
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'CFU',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Museo Moderno',
+                      ),
+                    )
+                  ],
                 ),
-                Text('Stato: ${_getStatus()}'),
+                Column(
+                  children: [
+                    widget.status
+                        ? HugeIcon(
+                            icon: HugeIcons.strokeRoundedCheckmarkBadge02,
+                            color: Colors.lightGreenAccent,
+                            size: 40,
+                          )
+                        : HugeIcon(
+                            icon: HugeIcons.strokeRoundedLoading02,
+                            color: Colors.lightBlueAccent,
+                            size: 40,
+                          ),
+                    Text(
+                      _getStatus(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Museo Moderno',
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Color(0xFFFC8D0A),
+                      child: Text(
+                        _getGrade(),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Museo Moderno',
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Voto',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Museo Moderno',
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
             SizedBox(
-              height: 20,
+              height: 30,
             ),
-            SizedBox(
-              height: 200,
-              child: SingleChildScrollView(
-                child: Text(
-                  widget.description,
+            Center(
+              child: Text(
+                'Descrizione :',
+                style: TextStyle(
+                  color: Color(0xFFFE2C8D),
+                  fontFamily: 'Museo Moderno',
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.5,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(17.0),
+                ),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      widget.description,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Museo Moderno',
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
             SizedBox(
               height: 20,
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        YoutubeScreen(searchName: widget.name),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            YoutubeScreen(searchName: widget.name),
+                      ),
+                    );
+                  },
+                  child: Row(
+                    children: [
+                      HugeIcon(
+                        icon: HugeIcons.strokeRoundedYoutube,
+                        color: Colors.red,
+                        size: 20,
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        'Video',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Museo Moderno',
+                        ),
+                      ),
+                    ],
                   ),
-                );
-              },
-              child: Text('Genera video'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => BookScreen(searchName: widget.name),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            BookScreen(searchName: widget.name),
+                      ),
+                    );
+                  },
+                  child: Row(
+                    children: [
+                      HugeIcon(
+                        icon: HugeIcons.strokeRoundedBookBookmark02,
+                        color: Colors.blue,
+                        size: 20,
+                      ),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        'Libri',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Museo Moderno',
+                        ),
+                      ),
+                    ],
                   ),
-                );
-              },
-              child: Text('Genera libri'),
+                ),
+              ],
             ),
           ],
         ),
