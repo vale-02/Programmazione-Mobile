af0547808bf5b7d91159052be211f3c79be42061
Valeria <mastrogiorgio.valeria45@gmail.com>
2024-11-03 14:10:03 +0100

    Personalizzazione del miniplayer dei video

--- brainiac/lib/youtube/video_screen.dart
+++ brainiac/lib/youtube/video_screen.dart
@@ -1,5 +1,6 @@
 import 'package:brainiac/model/video.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gradient_text/flutter_gradient_text.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideoScreen extends StatefulWidget {
@@ -35,7 +36,34 @@ class _VideoScreen extends State<VideoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _playerController.value.isFullScreen ? null : AppBar(),
+      appBar: _playerController.value.isFullScreen
+          ? null
+          : AppBar(
+              bottom: PreferredSize(
+                preferredSize: Size.fromHeight(1.0),
+                child: Container(
+                  height: 1,
+                  width: double.infinity,
+                  color: Colors.white,
+                ),
+              ),
+              centerTitle: true,
+              titleTextStyle: TextStyle(
+                fontSize: 20,
+                fontFamily: 'Museo Moderno',
+              ),
+              title: GradientText(
+                Text(
+                  widget.video.channelTitle,
+                ),
+                colors: [
+                  Color(0xFFFC8D0A),
+                  Color(0xFFFE2C8D),
+                ],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+              ),
+            ),
       body: PopScope(
         canPop: true,
         onPopInvokedWithResult: (didPop, result) {
@@ -66,15 +94,26 @@ class _VideoScreen extends State<VideoScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text(widget.video.title),
-              SizedBox(
-                height: 5,
+              Text(
+                widget.video.title,
+                style: TextStyle(
+                  color: Color(0xFFFC8D0A),
+                  fontSize: 15,
+                  fontFamily: 'Museo Moderno',
+                ),
+                textAlign: TextAlign.center,
               ),
-              Text(widget.video.channelTitle),
               SizedBox(
-                height: 5,
+                height: 10,
               ),
-              Text(widget.video.description),
+              Text(
+                widget.video.description,
+                style: TextStyle(
+                  fontSize: 12,
+                  fontFamily: 'Museo Moderno',
+                ),
+                textAlign: TextAlign.center,
+              ),
             ],
           ),
         ),
